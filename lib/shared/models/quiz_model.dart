26a7769a18befb38dc76010ec69da8c17b1c88d0
amazingustav <gustavoamorim02@gmail.com>
2021-04-25 02:29:22 -0300

    Create repository module to store data into JSON files

--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,6 +1,26 @@
+import 'dart:convert';
+
 import 'package:dev_quiz/shared/models/question_model.dart';
 
 enum Level { facil, medio, dificil, perito }
+
+extension LevelStringExt on String {
+  Level get levelParse => {
+        "facil": Level.facil,
+        "medio": Level.medio,
+        "dificil": Level.dificil,
+        "perito": Level.perito,
+      }[this]!;
+}
+
+extension LevelExt on Level {
+  String get parse => {
+        Level.facil: "facil",
+        Level.medio: "medio",
+        Level.dificil: "dificil",
+        Level.perito: "perito",
+      }[this]!;
+}
 
 class QuizModel {
   final String title;
@@ -16,4 +36,31 @@ class QuizModel {
     required this.image,
     required this.level,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'questions': questions.map((e) => e.toMap()).toList(),
+      'questionAnswered': questionAnswered,
+      'image': image,
+      'level': level.parse,
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      questions: List<QuestionModel>.from(
+        map['questions'].map((e) => QuestionModel.fromMap(e)),
+      ),
+      questionAnswered: map['questionAnswered'],
+      image: map['image'],
+      level: map['level'].toString().levelParse,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
 }
