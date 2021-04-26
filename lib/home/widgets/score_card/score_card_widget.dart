0274138ec16803a3b07962a0977027a7374c309c
amazingustav <gustavoamorim02@gmail.com>
2021-04-26 00:23:25 -0300

    Animate user score component

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -4,7 +4,9 @@ import 'package:dev_quiz/home/widgets/chart/chart_widget.dart';
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
-  const ScoreCardWidget({Key? key}) : super(key: key);
+  final double percentage;
+
+  const ScoreCardWidget({Key? key, required this.percentage}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +25,7 @@ class ScoreCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: ChartWidget(),
+                child: ChartWidget(percentage: percentage),
               ),
               Expanded(
                 flex: 3,
